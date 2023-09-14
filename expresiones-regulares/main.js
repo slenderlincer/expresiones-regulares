e7efb60d1ff291036d3c8703258ff757b5027e47
Saul Eliseo Aparicio Vivar <111158393+slenderlincer@users.noreply.github.com>
2023-09-14 00:56:11 -0700

    Update main.js

--- expresiones-regulares/main.js
+++ expresiones-regulares/main.js
@@ -1,23 +1,29 @@
 import './style.css'
 
-//Se define la palabra que sera la expresion regular
-let expresionRegular = /[01]/;
-
 //Se le asigna un evento al formulario de tipo "submit"
 const formulario = document.querySelector('#formulario');
 formulario.addEventListener("submit", mostrarValor);
 
 const parrafo = document.createElement('P');
+parrafo.classList.add('parrafo')
 
 //Funcion para mostrar el resultado en el DOM
 function mostrarValor(e){
   e.preventDefault();
+  
+  //Se define la expresion regular mediante entrada del usuario
+  const Expresionregular = document.querySelector('#regular-expresion').value;
+  const regularExpresion = new RegExp(`[${Expresionregular}]`);
+  console.log(regularExpresion);
 
+  
+  //Se define la cadena que sera testeada por la expresion regular.
   const cadena = document.querySelector('#cadena');
 
-  if(expresionRegular.test(cadena.value)){
+  if(regularExpresion.test(cadena.value)){
     parrafo.textContent = 'Hizo match';
 
+    return;
   }else{
     parrafo.textContent = 'No hizo match';
   }
@@ -25,3 +31,4 @@ function mostrarValor(e){
 }
 
 document.body.appendChild(parrafo);
+
